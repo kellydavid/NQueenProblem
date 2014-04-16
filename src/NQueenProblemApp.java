9e466bae2c61ae5dc24566eaa0864105b4e5c000
David Kelly <kellyd48@tcd.ie>
2014-04-16 17:27:33 +0100

    added debugging code

--- src/NQueenProblemApp.java
+++ src/NQueenProblemApp.java
@@ -19,11 +19,11 @@ public class NQueenProblemApp {
 	 */
 	public int N;
 	public int queens[]; // index of array is the row and value stored in array is the column
+	public int solveBoardCalls = 0;
 
 	public NQueenProblemApp(int N){
 		this.N = N;
 		queens = new int[N];
-		Arrays.fill(queens, 0); // fills the queens array with default -1 value
 	}
 
 	public boolean solve(){
@@ -31,14 +31,16 @@ public class NQueenProblemApp {
 	}
 
 	private boolean solveBoard(int queen){
+		solveBoardCalls++;
+		if(solveBoardCalls == 46)
+			System.out.println("break");
 		for(int i = 0; i < N; i++){		// loop through all the possible columns to place the queen
 			if(canPlace(queen, i)){		// find an empty column that has no attacking diagonals
 				queens[queen] = i;		// store the column number in the array of rows
 				if(queen == N - 1)		// if all the queens have been looped through return true
 					return true;
-				if(solveBoard(queen + 1)){ // if the deeper solveBoard method returns true then return true
+				if(solveBoard(queen + 1)) // if the deeper solveBoard method returns true then return true
 					return true;
-				}
 			}
 		}
 		queens[queen] = 0;
@@ -76,6 +78,11 @@ public class NQueenProblemApp {
 			}
 			string += "\n";
 		}
+		string += "\nQueen positions: ";
+	    for(int i = 0; i < queens.length; i++){
+	    	string += "(" + i + "," + queens[i] + ") ";
+	    }
+	    string += "\nSolve Board Method Calls: " + solveBoardCalls + "\n";
 		return string;
 	}
 }
